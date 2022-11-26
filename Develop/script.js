5b3327b2bc30c2d2aaed0f08ec0b2fe535bae61e
Ignacio Albistu <profiles.albistu@gmail.com>
2022-11-26 13:30:44 -0500

    Completed the Challenge

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,18 @@
+var specials = ["!","#","$","%","&","*", "+"," < ","="," > "," ? ","@"];
+//console.log(specials)
+var numbers = [0,1, 2, 3, 4, 5, 6, 7, 8, 9];
+//console.log(numbers)
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+//console.log(lowerCase)
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+//console.log(upperCase)
+
+var specialCriteria = false;
+var numbersCriteria = false;
+var lowercaseCriteria = false;
+var uppercaseCriteria = false;
+
+//BEGIN GIVEN CODE
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // Variables needed to be Defined
@@ -8,99 +23,68 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+
+// My CODE created with Bryan's help
+function generatePassword() {
+  var amountChars = parseInt(prompt("How many characters will your password contain (must be in-between 8 and 128)?"));
+    //console.log(typeof amountChars)
+    //console.log(amountChars)
+// Conditionals
+    if (!amountChars) {
+      alert("This needs a value");
+      return generatePassword();
+  } else if (amountChars < 8 || amountChars > 128) {
+      amountChars = parseInt(prompt("You must choose between 8 and 128"));
+      return generatePassword();
+  } else {
+      // Continues once user input is validated
+      numbersCriteria = confirm("Will this contain numbers?");
+      specialCriteria = confirm("Will this contain special characters?");
+      uppercaseCriteria = confirm("Will this contain Uppercase letters?");
+      lowercaseCriteria = confirm("Will this contain Lowercase letters?");
+  };
+   // Else if for 4 negative options
+   if (!numbersCriteria && !specialCriteria && !uppercaseCriteria && !lowercaseCriteria) {
+    choices = alert("You must choose a criteria!");
+    //this will start the process again
+    return generatePassword();
+  }
+  var userChoices = []
+  
+  
+   if (numbersCriteria) {
+    userChoices = userChoices.concat(numbers);
+    
+   }
+   if (specialCriteria) {
+    userChoices = userChoices.concat(specials);
+    
+   }
+   if (lowercaseCriteria) {
+    userChoices = userChoices.concat(lowerCase);
+  
+   }
+   if (uppercaseCriteria) {
+    userChoices = userChoices.concat(upperCase);
+    
+   }
+  
+
+var password = "";
+  // Start random selection variables:
+  // Random selection for all variables: 
+  for (var i = 0; i < amountChars; i++) {
+      var pickChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
+      // += also means concat, so password adds from pickchoices
+      password += pickChoices;
+  } 
+  // this takes our constructed password and brings it back to the function on line 21
+  return password;
+  }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Password Prompt Functions
-// Upper Characters Function
-function upperCase(length) {
-  generateBtn
-  let person = prompt("Will the password contain uppercase characters?");
-  var result = '';
-  var characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      }
-      return characters;
-    }  
-    console.log(upperCase);
-
-// Lowercase Characters Function
-function lowerCase(length) {
-  let person = prompt("Will the password contain lowercase characters?");
-  var result = '';
-  var characters ='abcdefghijklmnopqrstuvwxyz';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      }
-      return characters;
-    } console.log(lowerCase);
-
-// Numeric Characters Function
-function numberChars(length) {
-  let person = prompt("Will the password contain numeric characters?");
-  var result = '';
-  var characters ='0123456789';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      }
-      return characters;
-    } console.log(numberChars);
-
-// Special Characters Function
-function specialChars(length) {
-  let person = prompt("Will the password contain special characters?");
-  var result = '';
-  var characters ='!@#$%&*?+=';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-  return characters;
-} console.log(specialChars);
-
-// How many characters (8-128) Function
-function howManyChars() {
-  var numberChars = parseInt(prompt("How many characters would you like your password to have? Choose between 8 and 128"));
-  if (!numberChars) {
-    alert("This needs a value"); 
-} else if (numberChars > 8 || numberChars < 128) 
-  return numberChars;
-}console.log(howManyChars)
-
-//Master Function
-function generatePassword() {
-  let uCharacters = upperCase(length);
-  let lCharacters = lowerCase(length);
-  let nCharacters = numberChars(length);
-  let sCharacters = specialChars(length);
-  let characterSet = howManyChars(length);
-    if (uCharacters == null) {
-      alert("This needs a value");
-    } else (characterSet += uCharacters)
-    if (lCharacters == null) {
-      alert("This needs a value");
-    } else (characterSet += lCharacters)
-    if (nCharacters == null) {
-      alert("This needs a value");
-    } else (characterSet += nCharacters)
-    if (sCharacters == null) {
-      alert("This needs a value");
-    } else (characterSet += sCharacters)
-   console.log(characterSet)
-  var charactersLength = characterSet.length;
-  for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-  return characterSet;
-} console.log(generatePassword);
-
-
-
 
 
